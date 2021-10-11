c4255f7a73f10a8eef17fc730ded373aee36b47c
tykva43 <krikunova.olga@mail.ru>
2021-10-11 07:48:13 +0700

    Add necessary endpoints

--- server/urls.py
+++ server/urls.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, request
-from werkzeug.utils import redirect
 
 import business_logic as bl
 
 urls_blueprint = Blueprint('urls', __name__,)
 
 
-# Users endpoints
+# ============= Users endpoints =============
 @urls_blueprint.route('/users/<int:user_id>/', methods=['DELETE'])
 def remove_user(user_id):
     return bl.remove_user(user_id=user_id)
@@ -14,12 +13,12 @@ def remove_user(user_id):
 
 @urls_blueprint.route('/users/<int:user_id>/', methods=['POST'])
 def get_user(user_id):
-    return bl.get_users(user_id=user_id)
+    return bl.get_data(id=user_id, table_name='users')
 
 
 @urls_blueprint.route('/users/', methods=['POST'])
 def get_users_list():
-    return bl.get_users()
+    return bl.get_data(table_name='users')
 
 
 @urls_blueprint.route('/users/', methods=['PUT'])
@@ -41,3 +40,56 @@ def update_user(user_id):
     result = bl.update_user(user_id=user_id, user_data=request.form.to_dict(), photo_file=photo_file)
     return result
 
+
+# ============= Emails endpoints =============
+
+@urls_blueprint.route('/emails/<int:email_id>/', methods=['DELETE'])
+def remove_email(email_id):
+    return bl.remove_data(id=email_id, table_name='emails')
+
+
+@urls_blueprint.route('/emails/<int:email_id>/', methods=['POST'])
+def get_email(email_id):
+    return bl.get_data(id=email_id, table_name='emails')
+
+
+@urls_blueprint.route('/emails/', methods=['POST'])
+def get_emails_list():
+    return bl.get_data(table_name='emails')
+
+
+@urls_blueprint.route('/emails/', methods=['PUT'])
+def create_email():
+    return bl.create_data(data=request.form.to_dict(), table_name='emails')
+
+
+@urls_blueprint.route('/emails/<int:email_id>/', methods=['PATCH'])
+def update_email(email_id):
+    return bl.update_data(id=email_id, data=request.form.to_dict(), table_name='emails')
+
+
+# ============= Phones endpoints =============
+
+@urls_blueprint.route('/phones/<int:phone_id>/', methods=['DELETE'])
+def remove_phone(phone_id):
+    return bl.remove_data(id=phone_id, table_name='phones')
+
+
+@urls_blueprint.route('/phones/<int:phone_id>/', methods=['POST'])
+def get_phone(phone_id):
+    return bl.get_data(id=phone_id, table_name='phones')
+
+
+@urls_blueprint.route('/phones/', methods=['POST'])
+def get_phones_list():
+    return bl.get_data(table_name='phones')
+
+
+@urls_blueprint.route('/phones/', methods=['PUT'])
+def create_phone():
+    return bl.create_data(data=request.form.to_dict(), table_name='phones')
+
+
+@urls_blueprint.route('/phones/<int:phone_id>/', methods=['PATCH'])
+def update_phone(phone_id):
+    return bl.update_data(id=phone_id, data=request.form.to_dict(), table_name='phones')
